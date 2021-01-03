7252130c46c5a8e15ff12a0ce89422d371f6cfc2
Filip Staffa <294522+fstaffa@users.noreply.github.com>
2021-01-03 18:42:02 +0100

    Add project files.

---
+++ Libp2p.Tests/Connection/Multiselect/VarIntTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+using Libp2p.Connection.Multiselect;
+using NUnit.Framework;
+
+namespace Libp2p.Tests.Connection.Multiselect
+{
+    public class VarIntTests
+    {
+        [TestCaseSource(nameof(DecodeData))]
+        public ulong Decode_FullInput_ReturnsCorrectValue(byte[] input)
+        {
+            var str = PipeReader.Create(new MemoryStream(input));
+            return VarUInt.Decode(str);
+        }
+
+        private static IEnumerable<TestCaseData> DecodeData()
+        {
+            yield return new TestCaseData(new byte[] { 12 }).Returns(12);
+            yield return new TestCaseData(new byte[] { 172, 2 }).Returns(300);
+        }
+
+        [Test]
+        public void Decode_MissingFollowup_Throws()
+        {
+            var stream = PipeReader.Create(new MemoryStream(new byte[] { 172 }));
+            Assert.That(() => VarUInt.Decode(stream), Throws.Exception);
+        }
+
+        [TestCaseSource(nameof(EncodeData))]
+        public async Task<byte[]> Encode_ValidInput_ReturnsCorrectValue(ulong input)
+        {
+            var stream = new MemoryStream();
+            var writer = PipeWriter.Create(stream);
+            await VarUInt.Encode(input, writer);
+            return stream.ToArray();
+        }
+
+        private static IEnumerable<TestCaseData> EncodeData()
+        {
+            yield return new TestCaseData(12UL).Returns(new byte[] { 12 });
+            yield return new TestCaseData(300UL).Returns(new byte[] { 172, 2 });
+        }
+
+        [TestCase(2883UL)]
+        [TestCase(28798783UL)]
+        public async Task Encode_DecodeRoundtrip_ReturnsInput(ulong input)
+        {
+            var pipe = new Pipe();
+            await VarUInt.Encode(input, pipe.Writer);
+            Assert.That(VarUInt.Decode(pipe.Reader), Is.EqualTo(input));
+        }
+
+    }
+}
---
+++ Libp2p/Connection/Multiselect/VarUInt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Libp2p.Connection.Multiselect
+{
+    internal static class VarUInt
+    {
+        private const byte rest = 127;
+        private const byte mostSignificantBit = 128;
+        private const byte maxInputs = 9;
+        private const byte maxShift = 7 * maxInputs;
+
+        internal static ulong Decode(PipeReader reader)
+        {
+            ulong result = 0;
+            int current;
+            int shift = 0;
+            do
+            {
+                current = reader.AsStream().ReadByte();
+                if (current == -1)
+                {
+                    throw new InvalidDataException("Expected another byte for varint");
+                }
+
+                var add = (ulong)(((byte)((current) & rest)) << shift);
+                result += add;
+                shift += 7;
+            } while ((current & mostSignificantBit) == 128);
+
+            return result;
+        }
+
+        internal static async Task Encode(ulong number, PipeWriter writer)
+        {
+            var memory = writer.GetMemory(maxInputs);
+            int iteration = 0;
+            do
+            {
+                byte current = (byte)(number & rest);
+                number = number >> 7;
+                if (number > 0)
+                {
+                    current |= mostSignificantBit;
+                }
+                memory.Span[iteration] = current;
+                iteration++;
+            } while (number > 0);
+            writer.Advance(iteration);
+            await writer.FlushAsync();
+        }
+    }
+}
